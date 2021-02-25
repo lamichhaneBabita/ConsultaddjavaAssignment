1821a31fc860ec6efa8c6fed6a36016c694d0729
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-02-25 10:06:10 -0600

    Task two: Qn: 7 Added

--- tasktwo/ReadFloatNumber.java
+++ tasktwo/ReadFloatNumber.java
@@ -8,22 +8,20 @@ public class ReadFloatNumber {
         System.out.println("Enter float number: ");
         float num = sc.nextFloat();
 
-       if(num==0){
-           System.out.println("Zero");
-       }
-       if(num>1){
-           System.out.println("positive");
+        if (num == 0) {
+            System.out.println("Zero");
+        }
+        if (num > 1) {
+            System.out.println("positive");
 
-       }
-       else if(num<1) {
-           System.out.println("negative");
-       }
-       if(Math.abs(num)<1){
-           System.out.println("small");
-       }
-       else if(Math.abs(num)>1000000){
-           System.out.println("large");
-       }
-       sc.close();
+        } else if (num < 1) {
+            System.out.println("negative");
+        }
+        if (Math.abs(num) < 1) {
+            System.out.println("small");
+        } else if (Math.abs(num) > 1000000) {
+            System.out.println("large");
+        }
+        sc.close();
     }
 }
