d2934409abf4c9400529b62218647ba36f48b7c9
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-02-26 17:37:55 -0600

    Task 5: qn:2 Find Second Largest Element From Array.

--- task05/FindSecondLargestElement.java
+++ task05/FindSecondLargestElement.java
@@ -5,12 +5,18 @@ import java.util.Arrays;
 public class FindSecondLargestElement {
 
     public static void main(String[] args) {
-        int arr[] = {5, 3, 2, 1, 6};
+        int arr[] = {5, 3, 2, 1, 6, 6, 6};
         Arrays.sort(arr);
-        int num = 0;
-        num = arr[arr.length - 2];
-        System.out.println(num);
+        int temp = 0;
+        int max = arr[0];
 
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] != arr[arr.length - 1]) {
+                System.out.println(arr[i]);
+                return;
+            }
+        }
+        System.out.println("there is no element in array");
     }
 
 }
