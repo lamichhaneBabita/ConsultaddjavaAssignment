d210d38641b5bdc30edef14f52ebfa385e3d02fa
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-02-27 11:41:28 -0600

    Task 4: qn:8 Remove Duplicate value from array and return new array size.

---
+++ task05/NewLengthOfArray.java
@@ -0,0 +1,18 @@
+package task05;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NewLengthOfArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 23, 5, 2, 1, 5};
+
+        Set<Integer> set = new HashSet<>();
+
+        for (Integer se : arr) {
+            set.add(se);
+
+        }
+        System.out.println(set.size());
+    }
+}
