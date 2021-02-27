aa103e211d77a8442c5822e2866b4d3f359e34a6
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-02-26 23:25:27 -0600

    Task 4: qn:7 Differences between Largest And Smallest Num in Array.

--- task05/DifferencesOfLargestAndSmallestNum.java
+++ task05/DifferencesOfLargestAndSmallestNum.java
@@ -4,9 +4,18 @@ import java.util.Arrays;
 
 public class DifferencesOfLargestAndSmallestNum {
     public static void main(String[] args) {
-        int[] nums = {3, 4, 2, 6, 7, 8};
-        Arrays.sort(nums);
-        System.out.println("Differences between Largest " + nums[nums.length - 1] +
-                " and smallest " + nums[0] + " values in array is : " + (nums[nums.length - 1] - nums[0]));
+        int[] nums = {3,2,2,4,5,7,2,9};
+        if(nums.length==1){
+            System.out.println(nums[0]);
+            return;
+        }
+        if(nums.length>1) {
+            Arrays.sort(nums);
+            System.out.println("Differences between Largest " + nums[nums.length - 1] +
+                    " and smallest " + nums[0] + " values in array is : " + (nums[nums.length - 1] - nums[0]));
+        }
+        else{
+            System.out.println("Invalid Input");
+        }
     }
 }
