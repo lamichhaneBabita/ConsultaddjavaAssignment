8eb3d7b91279f52012152132cc87cd840802d3e8
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-02-27 19:03:05 -0600

    Task 4: qn:10 find pair whose sum is closest to zero in array

---
+++ task05/SumPairisClosestTo0.java
@@ -0,0 +1,22 @@
+package task05;
+
+public class SumPairisClosestTo0 {
+    public static void main(String[] args) {
+        int[] arr = {1, 30, -5, 70, -8, 20, -40, 60};
+        int sum1 = 0;
+        int sum2 = 1;
+        int min = arr[0] + arr[1];
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+                int tempSum = arr[i] + arr[j];
+                if (Math.abs(tempSum) < Math.abs(min)) {
+                    min = tempSum;
+                    sum1 = i;
+                    sum2 = j;
+                }
+            }
+        }
+        System.out.println(arr[sum1] + " " + arr[sum2]);
+    }
+}
