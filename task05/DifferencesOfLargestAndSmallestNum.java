d2942658dc3ba5b3629089bec7ea9197c39ab851
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-02-27 23:18:31 -0600

    Task 5: qn. 7 Largest and smallest differences.Updated

--- task05/DifferencesOfLargestAndSmallestNum.java
+++ task05/DifferencesOfLargestAndSmallestNum.java
@@ -11,8 +11,8 @@ public class DifferencesOfLargestAndSmallestNum {
         }
         if(nums.length>1) {
             Arrays.sort(nums);
-            System.out.println("Differences between Largest " + nums[nums.length - 1] +
-                    " and smallest " + nums[0] + " values in array is : " + (nums[nums.length - 1] - nums[0]));
+            System.out.println("Largest Value " + nums[nums.length - 1] +
+                    " - smallest value " + nums[0] + " in array is : " + (nums[nums.length - 1] - nums[0]));
         }
         else{
             System.out.println("Invalid Input");
