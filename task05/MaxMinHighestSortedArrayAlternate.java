3fa67b1a070b9aeeb77c82dbbead8d47af2aa475
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-02-27 19:48:25 -0600

    Task 4: qn:11 Re-arrange array max and min form

---
+++ task05/MaxMinHighestSortedArrayAlternate.java
@@ -0,0 +1,32 @@
+package task05;
+
+import java.util.Arrays;
+
+public class MaxMinHighestSortedArrayAlternate {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 4, 3, 6, 7, 5};
+        //    int[] temp =
+        MaxMinArray(arr);
+        //   System.out.println(Arrays.toString(temp));
+
+    }
+
+    public static void MaxMinArray(int[] arr) {
+        Arrays.sort(arr);
+        int[] temp = new int[arr.length];
+        boolean flag = true;
+        int largeValue = arr.length - 1;
+        int small = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (flag) {
+                temp[i] = arr[largeValue--];
+            } else {
+                temp[i] = arr[small++];
+            }
+            flag = !flag;
+        }
+
+        System.out.println(Arrays.toString(temp));
+
+    }
+}
