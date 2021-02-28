2911ef99a7c9ed5b7e137ea21bb37939ddb331e5
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-02-27 18:30:45 -0600

    Task 4: qn:9 Find Pair With given sum array

---
+++ task05/TwoElementSumToTarget.java
@@ -0,0 +1,20 @@
+package task05;
+
+public class TwoElementSumToTarget {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 6};
+        int k = 10;
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = j + 1; i < arr.length; i++) {
+                if (arr[i] + arr[j] == k) {
+                    System.out.println("Pair found at Index "+j + " and" + " at Index "+ i + " = " + k);
+                    return;
+                }
+
+            }
+
+        }
+        System.out.println("Pair not found");
+
+    }
+}
