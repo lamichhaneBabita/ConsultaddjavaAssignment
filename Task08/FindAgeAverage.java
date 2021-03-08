2fff8db8fdfb1dad6065116efa7674b45d9ee463
lamichhaneBabita <babita.lamichhane6@gmail.com>
2021-03-07 22:59:09 -0600

    Task 08: 8 Read CSV file and print Average Value.

---
+++ Task08/FindAgeAverage.java
@@ -0,0 +1,21 @@
+package Task08;
+
+import java.io.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class FindAgeAverage {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        InputStream is = new FileInputStream("test.csv");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(is));
+
+        //bf.lines().skip(1).map(s->s.split(",")[1]).forEach(System.out::println);
+
+        double avg = bf.lines().skip(1).map(s -> s.split(",")[1])
+                .mapToDouble(Double::parseDouble).average().getAsDouble();
+        System.out.println("Avg Value is: " + avg);
+
+    }
+}
